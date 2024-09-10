0c492a3aa935890cfedaca65c571aacd9ac96be6
Lucas Jardim <lucas.pasqualijardim59@gmail.com>
2024-09-10 11:18:17 -0300

    Criando comentarios com JavaDOC

--- src/Livro.java
+++ src/Livro.java
@@ -1,10 +1,24 @@
+/**
+ * Esta classe representa um Livro que esta na Biblioteca.
+ * Contém propriedades e métodos para manipular as informações de um livro.
+ *
+ * @author Lucas Jardim
+ */
 public class Livro {
 
+    // Propriedades do Livro
     private String titulo;
     private String autor;
     private int anoPublicacao;
     private boolean emprestado;
 
+    /**
+     * Criação do objeto Livro.
+     *
+     * @param titulo Título do livro
+     * @param autor Autor do livro
+     * @param anoPublicacao Ano de publicação do livro
+     */
     public Livro(String titulo, String autor, int anoPublicacao) {
         this.titulo = titulo;
         this.autor = autor;
@@ -12,22 +26,45 @@ public class Livro {
         this.emprestado = false;
     }
 
+    /**
+     * Retorna o título do livro.
+     *
+     * @return Título do livro
+     */
     public String getTitulo() {
         return titulo;
     }
 
+    /**
+     * Retorna o autor do livro.
+     *
+     * @return Autor do livro
+     */
     public String getAutor() {
         return autor;
     }
 
+    /**
+     * Retorna o ano de publicação do livro.
+     *
+     * @return o ano de publicação
+     */
     public int getAnoPublicacao() {
         return anoPublicacao;
     }
 
+    /**
+     * Verifica se o livro está emprestado.
+     *
+     * @return Retorna true se o livro estiver emprestado, false caso não estiver!
+     */
     public boolean isEmprestado() {
         return emprestado;
     }
 
+    /**
+     * Método para emprestar o livro.
+     */
     public void emprestar() {
         if (!emprestado) {
             emprestado = true;
@@ -37,6 +74,9 @@ public class Livro {
         }
     }
 
+    /**
+     * Método para devolver o livro.
+     */
     public void devolver() {
         if (emprestado) {
             emprestado = false;
@@ -46,15 +86,25 @@ public class Livro {
         }
     }
 
+    /**
+     * Método main para testar a classe Livro.
+     */
     public static void main(String[] args) {
+        // Criando um novo livro
         Livro livro1 = new Livro("O Senhor dos Anéis", "J.R.R. Tolkien", 1954);
 
+        // Printando o resultado
         System.out.println("Título: " + livro1.getTitulo());
         System.out.println("Autor: " + livro1.getAutor());
         System.out.println("Ano de Publicação: " + livro1.getAnoPublicacao());
 
+        // Emprestando o livro
         livro1.emprestar();
+
+        // Tentando emprestar novamente
         livro1.emprestar();
+
+        // Devolvendo o livro
         livro1.devolver();
     }
 }
